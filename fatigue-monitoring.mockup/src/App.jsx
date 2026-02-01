b4001f76a9c1d68b0f6b8c559c69cf5c29451148
UsepDeveloper <usep.developer@hotmail.com>
2026-02-01 21:40:14 +0700

    Add project files.

---
+++ fatigue-monitoring.mockup/src/App.jsx
@@ -0,0 +1,11 @@
+import SCCDashboard from './components/SCCDashboard'
+
+function App() {
+  return (
+    <div className="w-full h-screen overflow-hidden">
+      <SCCDashboard />
+    </div>
+  )
+}
+
+export default App
